bfa83bee6fa1ccaaf1c12e2113a2bb798c8a101c
Swayam Gurnule <swayamgurunule12@gmail.com>
2025-09-15 12:24:20 +0530

    White Pieces can move and get on one place and highlight places

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -21,19 +22,23 @@ public class GamePanel extends JPanel implements Runnable {
     final int FPS = 60;
     Thread gameThread;
     Board board = new Board();
+    Mouse mouse = new Mouse();
 
 // PIECES
-public static ArrayList<Piece> pieces = new ArrayList<>();
-public static ArrayList<Piece> simPieces = new ArrayList<>();
+    public static ArrayList<Piece> pieces = new ArrayList<>();
+    public static ArrayList<Piece> simPieces = new ArrayList<>();
+    Piece activeP;
 
 //COLOR 
     public static final int WHITE = 0;
     public static final int BLACK = 1;
-    int currentColor = WHITE ;
+    int currentColor = WHITE;
 
     public GamePanel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(Color.black);
+        addMouseMotionListener(mouse);
+        addMouseListener(mouse);
 
         setPieces();
         copyPieces(pieces, simPieces);
@@ -44,56 +49,54 @@ public static ArrayList<Piece> simPieces = new ArrayList<>();
         gameThread.start();
     }
 
-
     public void setPieces() {
-    // White team
-    pieces.add(new Pawn(WHITE, 0, 6));
-    pieces.add(new Pawn(WHITE, 1, 6));
-    pieces.add(new Pawn(WHITE, 2, 6));
-    pieces.add(new Pawn(WHITE, 3, 6));
-    pieces.add(new Pawn(WHITE, 4, 6));
-    pieces.add(new Pawn(WHITE, 5, 6));
-    pieces.add(new Pawn(WHITE, 6, 6));
-    pieces.add(new Pawn(WHITE, 7, 6));
-    pieces.add(new Rook(WHITE, 0, 7));
-    pieces.add(new Rook(WHITE, 7, 7));
-    pieces.add(new Knight(WHITE, 1, 7));
-    pieces.add(new Knight(WHITE, 6, 7));
-    pieces.add(new Bishop(WHITE, 2, 7));
-    pieces.add(new Bishop(WHITE, 5, 7));
-    pieces.add(new Queen(WHITE, 3, 7));
-    pieces.add(new King(WHITE, 4, 7));
+        // White team
+        pieces.add(new Pawn(WHITE, 0, 6));
+        pieces.add(new Pawn(WHITE, 1, 6));
+        pieces.add(new Pawn(WHITE, 2, 6));
+        pieces.add(new Pawn(WHITE, 3, 6));
+        pieces.add(new Pawn(WHITE, 4, 6));
+        pieces.add(new Pawn(WHITE, 5, 6));
+        pieces.add(new Pawn(WHITE, 6, 6));
+        pieces.add(new Pawn(WHITE, 7, 6));
+        pieces.add(new Rook(WHITE, 0, 7));
+        pieces.add(new Rook(WHITE, 7, 7));
+        pieces.add(new Knight(WHITE, 1, 7));
+        pieces.add(new Knight(WHITE, 6, 7));
+        pieces.add(new Bishop(WHITE, 2, 7));
+        pieces.add(new Bishop(WHITE, 5, 7));
+        pieces.add(new Queen(WHITE, 3, 7));
+        pieces.add(new King(WHITE, 4, 7));
 
-    // Black team
-    pieces.add(new Pawn(BLACK, 0, 1));
-    pieces.add(new Pawn(BLACK, 1, 1));
-    pieces.add(new Pawn(BLACK, 2, 1));
-    pieces.add(new Pawn(BLACK, 3, 1));
-    pieces.add(new Pawn(BLACK, 4, 1));
-    pieces.add(new Pawn(BLACK, 5, 1));
-    pieces.add(new Pawn(BLACK, 6, 1));
-    pieces.add(new Pawn(BLACK, 7, 1));
-    pieces.add(new Rook(BLACK, 0, 0));
-    pieces.add(new Rook(BLACK, 7, 0));
-    pieces.add(new Knight(BLACK, 1, 0));
-    pieces.add(new Knight(BLACK, 6, 0));
-    pieces.add(new Bishop(BLACK, 2, 0));
-    pieces.add(new Bishop(BLACK, 5, 0));
-    pieces.add(new Queen(BLACK, 3, 0));
-    pieces.add(new King(BLACK, 4, 0));
-}
-
-private void copyPieces(ArrayList<Piece> source, ArrayList<Piece> target) {
-    target.clear();
-    for (int i = 0; i < source.size(); i++) {
-        target.add(source.get(i)) ;
-
+        // Black team
+        pieces.add(new Pawn(BLACK, 0, 1));
+        pieces.add(new Pawn(BLACK, 1, 1));
+        pieces.add(new Pawn(BLACK, 2, 1));
+        pieces.add(new Pawn(BLACK, 3, 1));
+        pieces.add(new Pawn(BLACK, 4, 1));
+        pieces.add(new Pawn(BLACK, 5, 1));
+        pieces.add(new Pawn(BLACK, 6, 1));
+        pieces.add(new Pawn(BLACK, 7, 1));
+        pieces.add(new Rook(BLACK, 0, 0));
+        pieces.add(new Rook(BLACK, 7, 0));
+        pieces.add(new Knight(BLACK, 1, 0));
+        pieces.add(new Knight(BLACK, 6, 0));
+        pieces.add(new Bishop(BLACK, 2, 0));
+        pieces.add(new Bishop(BLACK, 5, 0));
+        pieces.add(new Queen(BLACK, 3, 0));
+        pieces.add(new King(BLACK, 4, 0));
     }
-}
+
+    private void copyPieces(ArrayList<Piece> source, ArrayList<Piece> target) {
+        target.clear();
+        for (int i = 0; i < source.size(); i++) {
+            target.add(source.get(i));
+
+        }
+    }
 
     @Override
-    public void run() 
-    {
+    public void run() {
 
         //GAME LOOP 
         double drawInterval = 1000000000 / FPS;
@@ -116,7 +119,41 @@ private void copyPieces(ArrayList<Piece> source, ArrayList<Piece> target) {
     }
 
     private void update() {
+        ///MOUSE BUTTON IS PRESSED ///
+        if (mouse.pressed == true) {
+            if (activeP == null) {
+                //if active piece s null  check if you can move piece
+                for (Piece piece : simPieces) {
+                    // if mouse is on ally Piece , pick it up as activeP
+                    if (piece.color == currentColor
+                            && piece.col == mouse.x / Board.SQUARE_SIZE
+                            && piece.row == mouse.y / Board.SQUARE_SIZE) {
+                        activeP = piece;
+                    }
+                }
+            } else {
+                // if Player is holding a piece , simulate the move 
+                simulate();
+            }
+        }
 
+        ///MOUSED IS PRESSED ///
+        if(mouse.pressed == false){
+            if(activeP !=null){
+                activeP.updatePosition();
+                activeP = null ;
+            }
+        }
+
+    }
+
+    private void simulate() {
+
+        //if piece is being held update , its postion 
+        activeP.x = mouse.x - Board.HALF_SQUARE_SIZE;
+        activeP.y = mouse.y - Board.HALF_SQUARE_SIZE;
+        activeP.col = activeP.getCol(activeP.x);
+        activeP.row = activeP.getRow(activeP.y);
     }
 
     public void paintComponent(Graphics g) {
@@ -128,8 +165,18 @@ private void copyPieces(ArrayList<Piece> source, ArrayList<Piece> target) {
         board.draw(g2);
 
         //PIECE
-        for (Piece p : simPieces){
+        for (Piece p : simPieces) {
             p.draw(g2);
+        }
+
+        if (activeP != null) {
+            g2.setColor(Color.white);
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.7f));
+            g2.fillRect(activeP.col * Board.SQUARE_SIZE, activeP.row * Board.SQUARE_SIZE, Board.SQUARE_SIZE, Board.SQUARE_SIZE);
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+
+            //draw active piece at end so it wont be hidden 
+            activeP.draw(g2);
         }
     }
 }
